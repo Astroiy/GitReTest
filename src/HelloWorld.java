b90abbd7ddad10065f09c0942785deb1627841ff
Tiny Chou <zjhxzzj163gd@163.com>
2022-04-29 00:33:14 +0800

    添加代码进入HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("This is my first time using git in IdealJ!");
+        System.out.println("Try again");
+        System.out.println("Recognize differences!");
     }
 }
